2ab9b568732cfafe2ee1a8dbecc200457d84c7b8
Franklin <Franklin.henrique.abs@gmail.com>
2025-07-02 17:57:07 -0300

    Add files via upload

---
+++ Trabalho_FDS.js
@@ -0,0 +1,6 @@
+let botao = document.querySelector("#botao");
+let textobotao = document.querySelector("#textobotao")
+botao.addEventListener("click", e=>{
+    textobotao.innerHTML="Saudações!";
+    textobotao.innerHTML="Seja bem vindo ao meu primeiro site! logo em seguida você tera mais algumas informações sobre mim!";
+})
